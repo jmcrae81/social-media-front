e6f70520eba91ffaae20e43210f363061024744b
jimmy <jimmy.a.mcrae@gmail.com>
2024-12-23 11:44:28 -0500

    v0.00002

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import LoginComponent from './LoginComponent';
 import RegistrationComponent from './RegistrationComponent';
 import HomepageComponent from './HomepageComponent';
 import PostComponent from './PostComponent';
+import NavbarComponent from './NavbarComponent';
 
 function App() {
   return (
     <div className="App">
         <PostComponent/>
+        <NavbarComponent/>
     </div>
   );
 }
--- src/NavbarComponent.js
+++ src/NavbarComponent.js
@@ -1,6 +1,15 @@
+import './Navbar.css';
+
 const NavbarComponent = () => {
     return(
-        <p>Links will be over here</p>
+        <div class="sidebar">
+            <p>Links will be over here</p>
+            <a>Find Friends</a>
+            <br/>
+            <a>Manage Profile</a>
+            <br/>
+            <a>Current Friends</a>
+        </div>
     );
 }
 
